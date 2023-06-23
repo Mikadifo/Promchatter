34c05fd5fc32116b118195e7b843ca81104dfc1c
Mikadifo <mikadifopadillaheredia@gmail.com>
2023-06-23 19:32:54 -0400

    add: navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -3,9 +3,15 @@ import logo from './../assets/imgs/logo.png';
 
 const Navbar = () => {
     return (
-        <nav className="bg-white text-black">
-            <Image src={logo} alt="Promchatter Logo" width={218} height={43} />
-            <ul className="font-lato">
+        <nav className="bg-purewhite text-black px-16 py-2 flex justify-between place-items-center sticky top-0 z-40 max-h-16 border-b border-solid border-gray">
+            <Image
+                src={logo}
+                alt="Promchatter Logo"
+                width={210}
+                height={45}
+                priority
+            />
+            <ul className="font-lato flex gap-3">
                 <li>
                     <a href="#">Explore</a>
                 </li>
@@ -15,10 +21,13 @@ const Navbar = () => {
                 <li>
                     <a href="#">About</a>
                 </li>
-                <li>
-                    <a href="#">Sign in</a>
-                </li>
             </ul>
+            <a
+                href="#"
+                className="px-6 py-3 border border-solid border-blue rounded-lg font-lato font-bold"
+            >
+                Sign in
+            </a>
         </nav>
     );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,15 @@ module.exports = {
         './src/app/**/*.{js,ts,jsx,tsx,mdx}',
     ],
     theme: {
+        colors: {
+            blue: '#1E4B8C',
+            black: 'rgba(0, 0, 0, 0.75)',
+            gray: '#808080',
+            white: '#F5F5F5',
+            purewhite: '#FFFFFF',
+            green: '#00A67C',
+            orange: '#FF8000',
+        },
         extend: {
             fontFamily: {
                 lato: ['var(--font-lato)'],
