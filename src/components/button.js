3c1ccc56d0f7a4dbc25cbf480d843dc8df0d1d26
Mikadifo <mikadifopadillaheredia@gmail.com>
2023-06-25 11:19:44 -0400

    mod: button font to bold

--- src/components/button.js
+++ src/components/button.js
@@ -1,6 +1,6 @@
 const Button = ({ children }) => {
     return (
-        <button className="bg-blue text-white px-6 py-3 rounded-lg shadow-bottom">
+        <button className="bg-blue text-white px-6 py-3 rounded-lg shadow-bottom font-bold">
             {children}
         </button>
     );
