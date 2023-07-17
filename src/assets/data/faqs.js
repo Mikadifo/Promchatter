91bace34cdd934fa3264c1895ffdf8b78cb9102b
Mikadifo <mikadifopadillaheredia@gmail.com>
2023-07-17 12:46:59 -0400

    add: faqs page

---
+++ src/assets/data/faqs.js
@@ -0,0 +1,30 @@
+//TODO: Migrate this to firebase DB
+const faqs = [
+    {
+        id: 'Q_prompt',
+        title: 'What is a prompt?',
+        answer: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
+    },
+    {
+        id: 'Q_build_prompt',
+        title: 'Are Promchatter prompts made by users?',
+        answer: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
+    },
+    {
+        id: 'Q_app_usability',
+        title: 'Do I need an account to use Promchatter?',
+        answer: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
+    },
+    {
+        id: 'Q_edit_prompts',
+        title: 'Can I edit prompts that are not mine?',
+        answer: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
+    },
+    {
+        id: 'Q_using_prompts',
+        title: 'Can i use a prompt in any AI-driven chat?',
+        answer: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
+    },
+];
+
+export default faqs;
